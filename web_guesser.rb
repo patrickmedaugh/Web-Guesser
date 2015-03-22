22f751ab8d273ab108f711a14fb324366234189f
Patrick Medaugh <patrickmedaugh@gmail.com>
2015-03-21 21:51:32 -0600

    Set up erb file and locals

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,7 +2,6 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-  erb :index
   number = rand(0..100)
-  "The secret number is #{number}"
+  erb :index, :locals => {:number => number}
 end
