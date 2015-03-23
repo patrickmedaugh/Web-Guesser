d9e035e4a392a5cc426a27013aca0554bb595d0a
Patrick Medaugh <patrickmedaugh@gmail.com>
2015-03-22 18:14:40 -0600

    I4: Guess Limiting extension

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,31 +2,50 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 @win = false
+@@guesses = 5
 def guess_check(guess)
-  if guess - 4 > SECRET_NUMBER
-    "Way too high!"
-  elsif guess + 4 < SECRET_NUMBER
-    "Way too low!"
-  elsif guess > SECRET_NUMBER
-    "Too high!"
-  elsif guess < SECRET_NUMBER
-    "Too low!"
+  if guess == 0 && @@guesses == 5
+     "Try to guess the secret number (0 - 100)!"
+  elsif guess - 4 > @@secret_number
+    "Way too high! #{@@guesses} guesses left..."
+  elsif guess + 4 < @@secret_number
+    "Way too low! #{@@guesses} guesses left..."
+  elsif guess > @@secret_number
+    "Too high! #{@@guesses} guesses left..."
+  elsif guess < @@secret_number
+    "Too low! #{@@guesses} guesses left..."
   else
     @win = true
-    "You got it right!"
+    "You got it right! Resetting..."
   end
+end
+
+def reset
+  if @win == true || @@guesses == 0
+    @@secret_number = rand(0..100)
+    @@guesses = 5
+  end
+end
+
+#guess_count
+if @@guesses == 0
+  reset
+else
+  @@guesses -= 1
 end
 
 def win_message
   if @win
-    "The SECRET NUMBER is #{SECRET_NUMBER}"
+    "The SECRET NUMBER is #{@@secret_number}"
+  elsif @@guesses == 0
+    "You are out of guesses! The SECRET NUMBER was #{@@secret_number}. Resetting number..."
   end
 end
 
-SECRET_NUMBER = rand(0..100)
+@@secret_number = rand(0..100)
 
 get '/' do
   guess = params[:guess].to_i
   message = guess_check(guess)
-  erb :index, :locals => {:number => SECRET_NUMBER, :message => message, :win_message => win_message}
+  erb :index, :locals => {:number => @@secret_number, :message => message, :win_message => win_message, :guess_count => guess_count}
 end
